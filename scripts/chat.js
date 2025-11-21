843095ebe9c8ba943e66af7e6d1061db6cf1aaec
Sharath Nataraj <sharath.nataraj@ipa.fraunhofer.de>
2025-11-21 10:16:28 +0100

    fix api support

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,4 +1,4 @@
-import { GEMINI_API_KEY } from '../robot_pick_place/config.js';
+import { GEMINI_API_KEY } from '.config.js';
 
 // Chatbot Logic
 
---
+++ scripts/config.js
@@ -0,0 +1,1 @@
+export const GEMINI_API_KEY = '__GEMINI_API_KEY__'; // Will be replaced by GitHub Actions
